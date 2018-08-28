d0e193a5370ab2b4a8dd298dd25f668f7108434a
Rob Parker <into.the.parker@gmail.com>
2018-08-28 12:14:51 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
-# Add  code here!
+def prime?(number)
+  if number < 0
+    return "Please enter a positive integer"
+
+  primeCheck = []
+    for counter in 2..number do
+        if number % counter == 0
+      primeCheck.push(counter)
+    end
+  end
+  return checker(primeCheck)
+end
+
+
+def checker(primeCheck)
+  if primeCheck.size > 1
+    return "FALSE"
+  else
+    return "TRUE"
+  end
+end
+prime?(11)
